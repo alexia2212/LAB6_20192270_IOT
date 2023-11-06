1873ac5c1e86fecee0dda2e0f1808a0f2a38927f
alexia2212 <a20192270@pucp.edu.pe>
2023-11-05 21:14:52 -0500

    f

--- app/src/main/java/com/example/lab6_20192270/JuegoPuzzle.java
+++ app/src/main/java/com/example/lab6_20192270/JuegoPuzzle.java
@@ -2,20 +2,59 @@ package com.example.lab6_20192270;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.Toast;
 
 import com.example.lab6_20192270.databinding.ActivityJuegoPuzzleBinding;
 
-public class JuegoPuzzle extends AppCompatActivity {
+import java.util.Random;
 
-    ActivityJuegoPuzzleBinding binding;
+public class JuegoPuzzle extends AppCompatActivity {
+    private int[] images = {R.drawable.crashflotante, R.drawable.gato, R.drawable.perrosalchicha};
+    private GridView gridView;
+    private Button btnAgregarImagen;
+    private ImagePagerAdapter imageAdapter;
+    private int[] drawableIds;
+    private boolean imageAdded = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityJuegoPuzzleBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_juego_puzzle);
+
+        gridView = findViewById(R.id.gridView);
+        btnAgregarImagen = findViewById(R.id.buttonAgregarPuzzle);
+
+        TypedArray imagesArray = getResources().obtainTypedArray(R.array.drawable_images);
+        drawableIds = new int[imagesArray.length()];
+        for (int i = 0; i < imagesArray.length(); i++) {
+            drawableIds[i] = imagesArray.getResourceId(i, 0);
+        }
+        imagesArray.recycle();
 
 
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (imageAdded) {
+                    // Solo permitir intercambiar piezas cuando se haya agregado una imagen
+                    // Implementa la lógica de intercambio de piezas aquí
+                    // Puedes usar las imágenes cargadas en el adaptador
+                } else {
+                    Toast.makeText(JuegoPuzzle.this, "Agrega una imagen primero", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
+
+    }
+    private int getRandomImageNumber() {
+        Random random = new Random();
+        int maxImageNumber = getResources().obtainTypedArray(R.array.drawable_images).length();
+        return random.nextInt(maxImageNumber) + 1;
     }
 }
